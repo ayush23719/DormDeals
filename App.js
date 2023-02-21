f10a35c7cb78f7cf057d58d29cdbefa6ed21e24e
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2023-02-21 19:39:08 +0530

    Starter Screen

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Image, Text, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class StarterScreen extends Component {
+  handleGetStarted = () => {
+    // Add any necessary code to navigate to the next screen
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source={require('./assets/image.png')} style={styles.image} />
+        <Text style={styles.description}>
+          Welcome to my app! This is a description of what my app does and how it can help you. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </Text>
+        <TouchableOpacity style={styles.button} onPress={this.handleGetStarted}>
+          <Text style={styles.buttonText}>Get Started</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +27,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+  description: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#007AFF',
+    borderRadius: 5,
+    padding: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
+
+export default StarterScreen;
