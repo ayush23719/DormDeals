80762362e9667afede0efbd64287526820897727
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2023-03-14 22:54:13 +0530

    Dashboard

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, Button, ScrollView, TouchableOpacity } from 'react-native';
 import firebase from '../database/firebase';
 import { useFonts } from 'expo-font';
+
 const Dashboard = ({ navigation }) => {
     const [fontsLoaded] = useFonts({
         'Raleway-Bold': require('../assets/fonts/static/Raleway-Bold.ttf'),
@@ -32,9 +33,17 @@ const Dashboard = ({ navigation }) => {
     return (
         <ScrollView contentContainerStyle={styles.container}>
             <Text style={styles.greetings}>Hello, {firstName}</Text>
-            <Text style={styles.welcome}>Looking to Buy, Sell or Donate?</Text>
-            <TouchableOpacity style={styles.button} onPress={signOut}>
-                <Text style={styles.buttonText}>Log Out</Text>
+            <View style={styles.buttonContainer}>
+                <Text style={styles.welcome}>Looking to Buy, Sell or Donate?</Text>
+                <TouchableOpacity style={styles.buy}>
+                    <Text style={styles.buyText}>Buy Items</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.sell}>
+                    <Text style={styles.sellText}>Sell Items</Text>
+                </TouchableOpacity>
+            </View>
+            <TouchableOpacity style={styles.logOut} onPress={signOut}>
+                <Text style={styles.logOutText}>Log Out</Text>
             </TouchableOpacity>
         </ScrollView>
     );
@@ -44,7 +53,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         display: "flex",
-        justifyContent: 'space-between',
         padding: 25,
         flexDirection: 'column',
     },
@@ -54,13 +62,47 @@ const styles = StyleSheet.create({
         marginTop: 50,
         marginRight: 100,
     },
+    buttonContainer: {
+        flexDirection: 'row',
+        marginTop: 30,
+        justifyContent: 'space-between'
+    },
     welcome: {
-        fontFamily: 'Raleway-Bold',
+        fontFamily: 'Raleway-Medium',
         fontSize: 20,
-        marginBottom: 420,
         marginRight: 50,
     },
-    buttonText: {
+    buy: {
+        marginLeft: -360,
+        marginTop: 80,
+        backgroundColor: '#000',
+        height: 85,
+        borderRadius: 15,
+        width: 150
+    },
+    buyText: {
+        fontFamily: 'Raleway-Bold',
+        color: '#fff',
+        marginTop: 25,
+        textAlign: 'center',
+        fontSize: 20,
+    },
+    sell: {
+        marginLeft: 20,
+        backgroundColor: '#000',
+        height: 85,
+        borderRadius: 15,
+        width: 150,
+        marginTop: 80,
+    },
+    sellText: {
+        fontFamily: 'Raleway-Bold',
+        color: '#fff',
+        marginTop: 25,
+        textAlign: 'center',
+        fontSize: 20,
+    },
+    logOutText: {
         fontFamily: 'Raleway-Bold',
         color: '#000',
         marginTop: 10,
@@ -68,8 +110,8 @@ const styles = StyleSheet.create({
         fontSize: 20,
 
     },
-    button: {
-        marginTop: 100,
+    logOut: {
+        marginTop: 350,
         backgroundColor: '#D4ED26',
         height: 55,
         borderRadius: 15,
