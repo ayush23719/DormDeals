9216f82c43e043221cc54827af2ff1b0b133405e
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2023-04-04 23:24:47 +0530

    gitignore

--- database/firebase.js
+++ database/firebase.js
@@ -1,16 +1,18 @@
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
+import Constants from 'expo-constants';
 const firebaseConfig = {
-    apiKey: "AIzaSyBB4K9qKnTcMmUxzDktkEDWXzMMSy0hdAk",
-    authDomain: "dormdeals-4937a.firebaseapp.com",
-    projectId: "dormdeals-4937a",
-    storageBucket: "dormdeals-4937a.appspot.com",
-    messagingSenderId: "655364243195",
-    appId: "1:655364243195:web:e3c6ae84202d4fa3413a5e",
-    measurementId: "G-JEXVJB95YP"
+    apiKey: Constants.manifest?.extra?.firebaseApiKey,
+    authDomain: Constants.manifest?.extra?.firebaseAuthDomain,
+    projectId: Constants.manifest?.extra?.firebaseProjectId,
+    storageBucket: Constants.manifest?.extra?.firebaseStorageBucket,
+    messagingSenderId: Constants.manifest?.extra?.firebaseMessagingSenderId,
+    appId: Constants.manifest?.extra?.firebaseAppId,
+    measurementId: Constants.manifest?.extra?.firebaseMeasurementId
 };
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length)
+    firebase.initializeApp(firebaseConfig);
 export default firebase;
 
 
