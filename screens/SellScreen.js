f39d29e349313962c6328848070ca6ae2779f21d
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2023-07-02 22:31:05 +0530

    -imagePicker

--- screens/SellScreen.js
+++ screens/SellScreen.js
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, ScrollView, Alert, ActivityIndicator, TouchableOpacity, Animated } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Alert } from 'react-native';
 import firebase from '../database/firebase';
-// import { Checkbox } from 'galio-framework';
 import { useFonts } from 'expo-font';
-// import * as ImagePicker from 'expo-image-picker';
-// import { Image } from 'react-native';
-import { NativeBaseProvider, extendTheme, Heading, Text, Checkbox, Link, Input, Button, Box, Flex } from 'native-base';
+import * as ImagePicker from 'expo-image-picker';
+import { Image } from 'react-native';
+import { NativeBaseProvider, extendTheme, Heading, Text, Checkbox, Link, Input, Button, Box, Flex, Center } from 'native-base';
 const Sell = ({ navigation }) => {
     const theme = extendTheme({
         fonts: {
@@ -48,10 +47,8 @@ const Sell = ({ navigation }) => {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [phone, setPhone] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
     const [isDonated, setIsDonated] = useState(false);
     const [image, setImage] = useState(null);
-    const [uploading, setUploading] = useState(false);
     const updateInputVal = (val, prop) => {
         if (prop === 'title') {
             setTitle(val);
@@ -64,39 +61,21 @@ const Sell = ({ navigation }) => {
 
     };
 
-    // const pickImage = async () => {
-    //     // No permissions request is necessary for launching the image library
-    //     let result = await ImagePicker.launchImageLibraryAsync({
-    //         mediaTypes: ImagePicker.MediaTypeOptions.All,
-    //         allowsEditing: true,
-    //         aspect: [4, 3],
-    //         quality: 1,
-    //     });
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
 
-    //     console.log(result);
+        console.log(result);
 
-    //     if (!result.canceled) {
-    //         setImage(result.assets[0].uri);
-    //     }
-    // };
+        if (!result.canceled) {
+            setImage(result.assets[0].uri);
+        }
+    };
 
-    // const uploadImage = async () => {
-    //     const blob = await new Promise((resolve, reject) => {
-    //         const xhr = new XMLHttpRequest();
-    //         xhr.onload = function () {
-    //             resolve(xhr.response);
-    //         };
-    //         xhr.onerror = function (e) {
-    //             reject(new TypeError("Network request failed"));
-    //         };
-    //         xhr.responseType = "blob";
-    //         xhr.open("GET", uri, true);
-    //         xhr.send(null);
-    //     });
-    // };
-    // const ref = firebase.storage().ref().child("images/" + image);
-    // const snapshot = ref.put(blob);
-    // snapshot.on(firebase.storage.TaskEvent.STATE_CHANGED)
     const sellItem = () => {
         setIsLoading(true);
         const userID = firebase.auth().currentUser.uid;
@@ -141,47 +120,16 @@ const Sell = ({ navigation }) => {
                 });
         }
     };
-
-    // const [titlePlaceholderPos] = useState(new Animated.Value(35));
-    // const [descriptionPlaceholderPos] = useState(new Animated.Value(35));
-    // const [phonePlaceholderPos] = useState(new Animated.Value(35));
-    // const [titlePlaceholderColor, setTitlePlaceholderColor] = useState('#828282');
-    // const [descriptionPlaceholderColor, setDescriptionPlaceholderColor] = useState('#828282');
-    // const [phonePlaceholderColor, setPhonePlaceholderColor] = useState('#828282');
-    // const [titlePlaceholderwidth, setTitlePlaceholderwidth] = useState(37);
-    // const [descriptionPlaceholderwidth, setDescriptionPlaceholderwidth] = useState(85);
-    // const [phonePlaceholderwidth, setPhonePlaceholderwidth] = useState(50);
-    // const [borderTitle, setBorderTitle] = useState('#c7c7c7');
-    // const [borderDescription, setBorderDescription] = useState('#c7c7c7');
-    // const [borderPhone, setBorderPhone] = useState('#c7c7c7');
-
-    // useEffect(() => {
-    //     setTitlePlaceholderColor(title === '' ? '#828282' : '#D4ED26');
-    //     setDescriptionPlaceholderColor(description === '' ? '#828282' : '#D4ED26');
-    //     setPhonePlaceholderColor(phone === '' ? '#828282' : '#D4ED26');
-    //     setTitlePlaceholderwidth(title === '' ? 37 : 37);
-    //     setDescriptionPlaceholderwidth(description === '' ? 85 : 85);
-    //     setPhonePlaceholderwidth(phone === '' ? 50 : 50);
-    //     setBorderTitle(title === '' ? '#c7c7c7' : '#D4ED26');
-    //     setBorderDescription(description === '' ? '#c7c7c7' : '#D4ED26');
-    //     setBorderPhone(phone === '' ? '#c7c7c7' : '#D4ED26');
-    // }, [title, description, phone]);
-
-
-    // if (isLoading) {
-    //     return (
-    //         <View style={styles.preloader}>
-    //             <ActivityIndicator size="large" color="#9E9E9E" />
-    //         </View>
-    //     );
-    // }
-
     return (
         <NativeBaseProvider theme={theme}>
             <Box flex={1} p={4} justifyContent="center" alignItems="center">
                 <View style={{ flex: 1, width: '100%', paddingTop: 20, paddingBottom: 20 }}>
-                    <Heading mb={4} fontSize="4xl">Sell Your Item</Heading>
-                    <Text fontSize="lg" mb={4}>List your item to sell in just a few steps. Just enter the following details!</Text>
+                    <Heading mb={4} fontSize="4xl">
+                        Sell Your Item
+                    </Heading>
+                    <Text fontSize="lg" mb={4}>
+                        List your item to sell in just a few steps. Just enter the following details!
+                    </Text>
                     <Box mb={2}>
                         <Input placeholder="Title" fontSize="md" value={title} onChangeText={(val) => updateInputVal(val, 'title')} />
                     </Box>
@@ -195,231 +143,31 @@ const Sell = ({ navigation }) => {
                     <Flex direction="row" alignItems="center" justifyContent="space-between" mb={4}>
                         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                             <Checkbox value="donate" colorScheme="primary" accessibilityLabel="Donated Items" size="md" onChange={(isChecked) => setIsDonated(isChecked)} />
-                            <Text ml={2} fontSize="md">Donated Items</Text>
+                            <Text ml={2} fontSize="md">
+                                Donate Item
+                            </Text>
                         </View>
-                        <Link _text={{ fontSize: "md" }}>Attach Image</Link>
+                        <Link _text={{ fontSize: 'md' }} onPress={pickImage}>
+                            Attach Image
+                        </Link>
                     </Flex>
-                    <Button size="lg" mb={4} onPress={() => sellItem()}>
-                        <Text style={{ fontSize: 18, color: 'white' }}>Post Item</Text>
-                    </Button>
+
+                    {image && (
+                        <Box alignItems="center" mb={4}>
+                            <Image source={{ uri: image }} style={{ width: 300, height: 150 }} />
+                        </Box>
+                    )}
+
+                    <Center>
+                        <Box height={60} width="100%" mb={4}>
+                            <Button flex={1} size="lg" onPress={() => sellItem()}>
+                                <Text style={{ fontSize: 18, color: 'white' }}>Post Item</Text>
+                            </Button>
+                        </Box>
+                    </Center>
                 </View>
             </Box>
         </NativeBaseProvider>
-
-        //         {/* <View style={styles.formGroup}>
-        //     <TouchableOpacity style={styles.image} onPress={pickImage}>
-        //         <Text style={styles.imageText}>Attach Image</Text>
-        //     </TouchableOpacity>
-        //     {image && <Image source={{ uri: image }} style={{ width: 20, height: 20 }} />}
-        //     <Animated.Text
-        //         style={[styles.placeholder, { marginLeft: 20, top: titlePlaceholderPos, color: titlePlaceholderColor, width: titlePlaceholderwidth }]}
-        //     >
-        //         Title
-        //     </Animated.Text>
-        //     <TextInput
-        //         style={[styles.inputStyle, { borderColor: borderTitle }]}
-        //         value={title}
-        //         onChangeText={(val) => updateInputVal(val, 'title')}
-        //         onFocus={() => {
-        //             {
-        //                 Animated.timing(titlePlaceholderPos, {
-        //                     toValue: 10,
-        //                     duration: 200,
-        //                     useNativeDriver: false,
-        //                 }).start();
-        //             }; setTitlePlaceholderColor('#D4ED26'); setTitlePlaceholderwidth(37);
-        //             setBorderTitle('#D4ED26');
-        //         }}
-        //         onBlur={() => {
-        //             {
-        //                 if (title.length === 0) {
-        //                     Animated.timing(titlePlaceholderPos, {
-        //                         toValue: 35,
-        //                         duration: 200,
-        //                         useNativeDriver: false,
-        //                     }).start();
-        //                 }
-        //             }; setTitlePlaceholderColor(title === '' ? '#828282' : '#D4ED26');
-        //             setBorderTitle(title === '' ? '#c7c7c7' : '#D4ED26')
-        //         }}
-        //     />
-        //     <Animated.Text
-        //         style={[styles.placeholder, { marginLeft: 20, top: descriptionPlaceholderPos, color: descriptionPlaceholderColor, width: descriptionPlaceholderwidth }]}
-        //     >
-        //         Description
-        //     </Animated.Text>
-        //     <TextInput
-        //         style={[styles.inputStyle, { borderColor: borderDescription, height: 150, padding: 15, textAlignVertical: 'top' }]}
-        //         value={description}
-        //         onChangeText={(val) => updateInputVal(val, 'description')}
-        //         multiline={true}
-        //         numberOfLines={8}
-        //         onFocus={() => {
-        //             {
-        //                 Animated.timing(descriptionPlaceholderPos, {
-        //                     toValue: 10,
-        //                     duration: 200,
-        //                     useNativeDriver: false,
-        //                 }).start();
-        //             }; setDescriptionPlaceholderColor('#D4ED26'); setDescriptionPlaceholderwidth(85);
-        //             setBorderDescription('#D4ED26');
-        //         }}
-        //         onBlur={() => {
-        //             {
-        //                 if (description.length === 0) {
-        //                     Animated.timing(descriptionPlaceholderPos, {
-        //                         toValue: 35,
-        //                         duration: 200,
-        //                         useNativeDriver: false,
-        //                     }).start();
-        //                 }
-        //             }; setDescriptionPlaceholderColor(description === '' ? '#828282' : '#D4ED26');
-        //             setBorderDescription(description === '' ? '#c7c7c7' : '#D4ED26')
-        //         }} />
-        //     <Animated.Text
-        //         style={[styles.placeholder, { marginLeft: 20, top: phonePlaceholderPos, color: phonePlaceholderColor, width: phonePlaceholderwidth }]}
-        //     >
-        //         Phone
-        //     </Animated.Text>
-        //     <TextInput
-        //         style={[styles.inputStyle, { borderColor: borderPhone }]}
-        //         value={phone}
-        //         onChangeText={(val) => updateInputVal(val, 'phone')}
-
-
-        //         onFocus={() => {
-        //             {
-        //                 Animated.timing(phonePlaceholderPos, {
-        //                     toValue: 10,
-        //                     duration: 200,
-        //                     useNativeDriver: false,
-        //                 }).start();
-        //             }; setPhonePlaceholderColor('#D4ED26'); setPhonePlaceholderwidth(50);
-        //             setBorderPhone('#D4ED26');
-        //         }}
-        //         onBlur={() => {
-        //             {
-        //                 if (description.length === 0) {
-        //                     Animated.timing(phonePlaceholderPos, {
-        //                         toValue: 35,
-        //                         duration: 200,
-        //                         useNativeDriver: false,
-        //                     }).start();
-        //                 }
-        //             }; setPhonePlaceholderColor(phone === '' ? '#828282' : '#D4ED26');
-        //             setBorderPhone(phone === '' ? '#c7c7c7' : '#D4ED26')
-        //         }} />
-        //     <Checkbox color="success" label="Donate This Item" checkboxStyle={styles.checkbox} labelStyle={styles.label} onChange={(isChecked) => setIsDonated(isChecked)} />
-
-        //     <TouchableOpacity style={styles.button} onPress={() => { sellItem; uploadImage }}>
-        //         <Text style={styles.buttonText}>Post Item</Text>
-        //     </TouchableOpacity>
-
-        // </View> */}
-
-
     );
 };
-
-const styles = StyleSheet.create({
-    // container: {
-    //     flex: 1,
-    //     display: "flex",
-    //     flexDirection: "column",
-    //     padding: 20,
-    //     backgroundColor: '#fff',
-    //     overflowY: 'auto'
-    // },
-    // header: {
-    //     marginTop: -30,
-    // },
-    // headerText: {
-    //     fontFamily: 'Raleway-Bold',
-    //     fontSize: 35,
-    //     marginBottom: 10
-    // },
-    // headerDesc: {
-    //     fontFamily: 'Raleway',
-    //     fontSize: 16,
-    //     color: '#828282'
-    // },
-    // formGroup: {
-    //     marginTop: 30,
-    // },
-    // placeholder: {
-    //     zIndex: 1,
-    //     backgroundColor: '#fff',
-    //     fontFamily: 'Raleway',
-    //     fontSize: 15,
-    // },
-    // inputStyle: {
-    //     height: 55,
-    //     width: '100%',
-    //     marginBottom: 0,
-    //     paddingLeft: 20,
-    //     alignSelf: "center",
-    //     borderColor: "#c7c7c7",
-    //     borderWidth: 1,
-    //     fontFamily: 'Raleway',
-    //     fontSize: 15,
-    //     borderRadius: 15
-
-    // },
-    // image: {
-    //     marginTop: 30,
-    //     backgroundColor: '#000',
-    //     height: 55,
-    //     borderRadius: 15,
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     width: '50%',
-    //     alignSelf: 'center'
-    // },
-    // imageText: {
-    //     fontFamily: 'Raleway-Bold',
-    //     color: '#fff',
-    //     fontSize: 20,
-    //     marginTop: -10,
-    // },
-    // buttonText: {
-    //     fontFamily: 'Raleway-Bold',
-    //     color: '#000',
-    //     marginTop: 10,
-    //     textAlign: 'center',
-    //     fontSize: 20,
-
-    // },
-    // button: {
-    //     marginTop: 60,
-    //     backgroundColor: '#D4ED26',
-    //     height: 55,
-    //     borderRadius: 15,
-    // },
-    // preloader: {
-    //     left: 0,
-    //     right: 0,
-    //     top: 0,
-    //     bottom: 0,
-    //     position: 'absolute',
-    //     alignItems: 'center',
-    //     justifyContent: 'center',
-    //     backgroundColor: '#fff'
-    // },
-    // checkbox: {
-    //     marginTop: 10,
-    //     marginLeft: 10,
-    // },
-    // label: {
-    //     fontFamily: 'Raleway',
-    //     fontSize: 15,
-    //     marginTop: 10,
-    //     marginLeft: 10
-    // },
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-});
-
 export default Sell;
